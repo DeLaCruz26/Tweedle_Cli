e9dcf7d2e975c9c215d784943b492b1865c12e3d
DeLaCruz26 <dametrik.fick@gmail.com>
2020-08-03 18:30:17 -0400

    updated files

--- lib/tweedle_cli.rb
+++ lib/tweedle_cli.rb
@@ -1,9 +1,11 @@
 require_relative './tweedle_cli/version'
 require_relative './tweedle_cli/cli'
-require_relative './tweedle_cli/flower'
+require_relative './tweedle_cli/hemp'
+require_relative './tweedle_cli/flowers'
 require_relative './tweedle_cli/scraper'
 
 require 'pry'
 require 'nokogiri'
 require 'open-uri'
+require 'colorize'
 
--- lib/tweedle_cli/cli.rb
+++ lib/tweedle_cli/cli.rb
@@ -1,67 +1,43 @@
 class TweedleCli::CLI
 
     def call
-        puts "Welcome to Tweedle!"
-        get_sorted_options
+        puts "\nWelcome to Tweedle!\n".colorize(:green)
+        get_options
         list_options
-        #get_flowers_from(sort_by)
-        #list_flowers
-        #select_flower
-        #puts "If you would like to see the recent list of Hemp Flower, enter 'cbd'"
-        #puts "To exit, type 'exit'"
-        #menu
+        get_user_option
     end
+        
 
     def list_options
-        #list options
-        @sort_by.each.with_index(1) do |option, index| 
-            puts "#{index}. #{option}"
+        puts "\nChoose an option to see flowers.\n"
+        puts ""
+        @options.each.with_index(1) do |option, index| 
+            puts "#{index}. #{option.name}"
         end
     end
 
-    def get_sorted_options
-        #to be scraped
-        @sort_by = ["Featured Items", "Newest Items", "Best Selling", "A to Z", "Z to A", "Price: Ascending", "Price: Descending"]
-
+    def get_options
+        @options = TweedleCli::HempFlower.all   
     end
 
-    #def menu 
-    #    input = gets.strip.downcase
-#
-    #    if input == "cbd"
-    #        flower_list
-    #        menu
-    #    elsif input == "exit"
-    #        goodbye
-    #    else
-    #        invalid_entry
-    #    end
-    #end
+    def get_user_option
+        user_option = gets.strip.to_i
         
-        #def invalid_entry
-        #    puts "Invalid entry, try again!"
-        #    menu
-        #end
+        if valid(user_option, @options)
+            show_flowers(user_option)
+        end
+    end
 
-        #def flower_list
-         #   puts "1. Cheese CBD"
-         #   puts "2. Sour Space Candy"
-        #    puts "4. White CBG Smalls"
-        #    puts "5. Berry Blossom"
-        #    puts ""
-        #    puts "Enter number for more detail"
-        #    input = gets.strip.downcase
+    def valid(input, options)
+        input <= options.length && input > 0
+    end
 
-        #    select_flower(input)
-        #end
+    def show_flowers(user_option)
+        option = @options[user_option - 1]
+        flowers = option.flowers
+        puts "#{option.name}"
+    end 
 
-        #def select_flower(flower)
-        #    puts "#{flower}"
-        #    #go over array and find by index number.
-        #end
-        
-        #def goodbye 
-        #    puts "Goodbye, come back for more cbd!"
-        #end
+    
 
 end
