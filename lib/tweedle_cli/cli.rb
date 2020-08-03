de051163d6d16bd30fa34731236872bf8cb08ba6
DeLaCruz26 <59461151+DeLaCruz26@users.noreply.github.com>
2020-08-03 18:24:42 -0400

    Update cli.rb
    Refactered cli class

--- lib/tweedle_cli/cli.rb
+++ lib/tweedle_cli/cli.rb
@@ -1,28 +1,43 @@
 class TweedleCli::CLI
 
     def call
-        puts "Welcome to Tweedle!"
-        get_sorted_options
+        puts "\nWelcome to Tweedle!\n".colorize(:green)
+        get_options
         list_options
-        #get_flowers_from(sort_by)
-        #list_flowers
-        #select_flower
-        #puts "If you would like to see the recent list of Hemp Flower, enter 'cbd'"
-        #puts "To exit, type 'exit'"
-        #menu
+        get_user_option
     end
+        
 
     def list_options
-        #list options
-        @sort_by.each.with_index(1) do |option, index| 
-            puts "#{index}. #{option}"
+        puts "\nChoose an option to see flowers.\n"
+        puts ""
+        @options.each.with_index(1) do |option, index| 
+            puts "#{index}. #{option.name}"
         end
     end
 
-    def get_sorted_options
-        #to be scraped
-        @sort_by = ["Featured Items", "Newest Items", "Best Selling", "A to Z", "Z to A", "Price: Ascending", "Price: Descending"]
-
+    def get_options
+        @options = TweedleCli::HempFlower.all   
     end
+
+    def get_user_option
+        user_option = gets.strip.to_i
+        
+        if valid(user_option, @options)
+            show_flowers(user_option)
+        end
+    end
+
+    def valid(input, options)
+        input <= options.length && input > 0
+    end
+
+    def show_flowers(user_option)
+        option = @options[user_option - 1]
+        flowers = option.flowers
+        puts "#{option.name}"
+    end 
+
+    
 
 end
