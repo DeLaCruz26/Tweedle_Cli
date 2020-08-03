85be81bac2aa2e10dd41bbafb5c0cfac2bf99a41
DeLaCruz26 <59461151+DeLaCruz26@users.noreply.github.com>
2020-08-03 18:25:43 -0400

    Update scraper.rb

--- lib/tweedle_cli/scraper.rb
+++ lib/tweedle_cli/scraper.rb
@@ -1,10 +1,21 @@
-class Scraper
+class TweedleCli::Scraper
 
-    def self.scrape_flowers
+    def self.scrape_options
         site = "https://tweedlefarms.com/hemp-flower/"
 
         doc = Nokogiri::HTML(open(site))
-        binding.pry
+
+        options = doc.css(".form-select.form-select--small option")
+
+        options.each do |opt|
+            name = opt.text
+            TweedleCli::HempFlower.new(name)
+        end
+
+        def self.scrape_flowers(flower)
+    
+        end
+      
     end
 
 end
