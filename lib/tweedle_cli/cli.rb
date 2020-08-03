c388b5daee8a628ecd47ba202f1596f99c43d339
DeLaCruz26 <59461151+DeLaCruz26@users.noreply.github.com>
2020-08-03 18:24:03 -0400

    Update cli.rb

--- lib/tweedle_cli/cli.rb
+++ lib/tweedle_cli/cli.rb
@@ -25,43 +25,4 @@ class TweedleCli::CLI
 
     end
 
-    #def menu 
-    #    input = gets.strip.downcase
-#
-    #    if input == "cbd"
-    #        flower_list
-    #        menu
-    #    elsif input == "exit"
-    #        goodbye
-    #    else
-    #        invalid_entry
-    #    end
-    #end
-        
-        #def invalid_entry
-        #    puts "Invalid entry, try again!"
-        #    menu
-        #end
-
-        #def flower_list
-         #   puts "1. Cheese CBD"
-         #   puts "2. Sour Space Candy"
-        #    puts "4. White CBG Smalls"
-        #    puts "5. Berry Blossom"
-        #    puts ""
-        #    puts "Enter number for more detail"
-        #    input = gets.strip.downcase
-
-        #    select_flower(input)
-        #end
-
-        #def select_flower(flower)
-        #    puts "#{flower}"
-        #    #go over array and find by index number.
-        #end
-        
-        #def goodbye 
-        #    puts "Goodbye, come back for more cbd!"
-        #end
-
 end
